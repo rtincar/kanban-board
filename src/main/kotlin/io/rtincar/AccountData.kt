d7fb1c9ae5aee84601322aa856e18f108d4d7bf2
rtincar <ul2mac@gmail.com>
2017-09-25 20:17:54 +0200

    [US-001] El usuario se registra en la aplicación

--- src/main/kotlin/io/rtincar/AccountData.kt
+++ src/main/kotlin/io/rtincar/AccountData.kt
@@ -1,7 +1,5 @@
 package io.rtincar
 
-import java.util.regex.Pattern
-
 class AccountData(
         val email: String,
         val firstName: String,
@@ -10,20 +8,57 @@ class AccountData(
         val passwordConfirmation: String) {
 
 
-
     fun validate() {
         validateEmail()
+        validateFirstName()
+        validateLastName()
+        validatePassword()
         validatePasswordConfirmation()
     }
 
+
+    private fun validateFirstName() {
+        if (firstName.length < 2)
+            throw InvalidAccountDataException("First name should contain at least 2 characters")
+    }
+
+    private fun validateLastName() {
+        if (lastName.length < 2)
+            throw InvalidAccountDataException("Last name should contain at least 2 characters")
+    }
+
+    private fun validatePassword() {
+        if (passwordHasNotMinLength()
+                || passwordHasNotContainOneLowerCaseChar()
+                || passwordHasNotContainOneUpperCaseCharacter()
+                || passwordHasNotContainOneDigit()
+                || passwordHasNotContainOneSpecialCharacter())
+            throw InvalidAccountDataException("Invalid password")
+
+    }
+
+    private fun passwordHasNotMinLength() = password.length < 8
+
+    private fun passwordHasNotContainOneLowerCaseChar() = !validateRegex(password, "(.*)([a-z]+)(.*)")
+
+    private fun passwordHasNotContainOneUpperCaseCharacter() = !validateRegex(password, "(.*)([A-Z]+)(.*)")
+
+    private fun passwordHasNotContainOneDigit() = !validateRegex(password, "(.*)([0-9]+)(.*)")
+
+    private fun passwordHasNotContainOneSpecialCharacter() = !validateRegex(password, "(.*)([!-/:-@]+)(.*)")
+
     private fun validateEmail() {
-        val emailPattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})\$")
-        if (!emailPattern.matcher(email).matches()) throw InvalidAccountDataException("Invalid email")
+        val emailRegex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})\$"
+        if (!validateRegex(email, emailRegex))
+            throw InvalidAccountDataException("Invalid email")
     }
 
     private fun validatePasswordConfirmation() {
-        if (password != passwordConfirmation) throw InvalidAccountDataException("Confirmation password doesn't match")
+        if (password != passwordConfirmation)
+            throw InvalidAccountDataException("Confirmation password doesn't match")
     }
+
+    private fun validateRegex(str: String, regex: String): Boolean = str.matches(Regex(regex))
 
 
 }
--- src/test/kotlin/io/rtincar/AccountDataTests.kt
+++ src/test/kotlin/io/rtincar/AccountDataTests.kt
@@ -3,35 +3,87 @@ package io.rtincar
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.function.Executable
 
 @DisplayName("AccountData Tests")
 class AccountDataTests {
 
+    private val validPassword = "a8Akls@)s"
+
     @Test
     @DisplayName("Validate wrong format email")
     fun validateWrongEmail() {
-        val accountData = AccountData("", "John", "Doe", "pass123", "pass123")
-        val assertThrows= Assertions.assertThrows(InvalidAccountDataException::class.java, {
-            accountData.validate()
-        })
-        Assertions.assertEquals("Invalid email", assertThrows.message)
+        val accountData = AccountData("", "John", "Doe", validPassword, validPassword)
+        assertValidateException(accountData, "Invalid email")
+
     }
 
     @Test
     @DisplayName("Validate right email format")
     fun validateRightEmail() {
-        val accountData = AccountData("account@domain.com", "John", "Doe", "pass123", "pass123")
+        val accountData = AccountData("account@domain.com", "John", "Doe", validPassword, validPassword)
         accountData.validate()
     }
 
     @Test
-    @DisplayName("Validate password confirmation")
+    @DisplayName("Validate password")
     fun validatePassword() {
-        val accountData = AccountData("account@domain.com", "John", "Doe", "pass123", "pass12")
+
+
+        Assertions.assertAll("Invalid passwords",
+                Executable {
+                    val accountData = createAccountDataWithPassword("aaaaaaa")
+                    assertValidateException(accountData, "Invalid password")
+                },
+                Executable {
+                    val accountData = createAccountDataWithPassword("aaaaaaaa")
+                    assertValidateException(accountData, "Invalid password")
+                },
+                Executable {
+                    val accountData = createAccountDataWithPassword("aaAaaaaa")
+                    assertValidateException(accountData, "Invalid password")
+                },
+                Executable {
+                    val accountData = createAccountDataWithPassword("a2Aaaaaa")
+                    assertValidateException(accountData, "Invalid password")
+                }
+        )
+
+        val accountData = createAccountDataWithPassword(validPassword)
+        accountData.validate()
+
+    }
+
+    @Test
+    @DisplayName("Validate password confirmation")
+    fun validatePasswordConfirmation() {
+        val accountData = AccountData("account@domain.com", "John", "Doe", validPassword, "a8Akls@)k")
+        assertValidateException(accountData, "Confirmation password doesn't match")
+    }
+
+
+    @Test
+    @DisplayName("Validate first name")
+    fun validateFirstName() {
+        val accountData = AccountData("account@domain.com", "a", "Doe", validPassword, "a8Akls@)k")
+        assertValidateException(accountData, "First name should contain at least 2 characters")
+    }
+
+    @Test
+    @DisplayName("Validate last name")
+    fun validateLastName() {
+        val accountData = AccountData("account@domain.com", "John", "s", validPassword, "a8Akls@)k")
+        assertValidateException(accountData, "Last name should contain at least 2 characters")
+    }
+
+    private fun createAccountDataWithPassword(password: String): AccountData =
+            AccountData("account@domain.com", "John", "Doe", password, password)
+
+    private fun assertValidateException(accountData: AccountData, message: String) {
         val assertThrows= Assertions.assertThrows(InvalidAccountDataException::class.java, {
             accountData.validate()
         })
-        Assertions.assertEquals("Confirmation password doesn't match", assertThrows.message)
+        Assertions.assertEquals(message, assertThrows.message)
     }
 
 }
