b2482cb618a96d29c276f96d4f5d1246c07fe0cd
rtincar <ul2mac@gmail.com>
2017-11-12 21:28:50 +0100

    [ARQ] - Configuración del proyecto: Actualizar a Spring Boot 2.0.0.M6

--- application/web/src/main/kotlin/io/rtincar/kanbanboard/web/AccountHandler.kt
+++ application/web/src/main/kotlin/io/rtincar/kanbanboard/web/AccountHandler.kt
@@ -3,9 +3,11 @@ package io.rtincar.kanbanboard.web
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.rtincar.kanbanboard.account.AccountData
 import io.rtincar.kanbanboard.account.AccountManager
+import org.springframework.http.HttpStatus
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.ok
+import org.springframework.web.reactive.function.server.ServerResponse.status
 import org.springframework.web.reactive.function.server.body
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
@@ -15,7 +17,7 @@ class AccountHandler(private val accountManager: AccountManager) {
     fun createAccount(req: ServerRequest): Mono<ServerResponse> {
         val accountData = AccountData("domain@account.com", "First name", "Last name", "4#Mooooo", "4#Mooooo")
         val account = accountManager.createAccount(accountData)
-        return ok().body(ObjectMapper().writeValueAsString(account).toMono())
+        return status(HttpStatus.CREATED).body(ObjectMapper().writeValueAsString(account).toMono())
     }
 
 }
