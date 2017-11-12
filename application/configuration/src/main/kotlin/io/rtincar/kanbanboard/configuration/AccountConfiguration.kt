941d29ff1a43291528c823b61bf3ae57c8d82855
rtincar <ul2mac@gmail.com>
2017-11-12 21:05:39 +0100

    [ARQ] - Configuración del proyecto: Actualizar a Spring Boot 2.0.0.M6

--- application/configuration/src/main/kotlin/io/rtincar/kanbanboard/configuration/AccountConfiguration.kt
+++ application/configuration/src/main/kotlin/io/rtincar/kanbanboard/configuration/AccountConfiguration.kt
@@ -7,8 +7,10 @@ import io.rtincar.kanbanboard.web.AccountHandler
 import io.rtincar.kanbanboard.web.AccountRouter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.EnableWebFlux
 
 @Configuration
+@EnableWebFlux
 class AccountConfiguration {
 
     @Bean
--- application/configuration/src/main/kotlin/io/rtincar/kanbanboard/configuration/Application.kt
+++ application/configuration/src/main/kotlin/io/rtincar/kanbanboard/configuration/Application.kt
@@ -1,13 +1,11 @@
 package io.rtincar.kanbanboard.configuration
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableWebFlux
 class Application
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    runApplication<Application>(*args)
 }
