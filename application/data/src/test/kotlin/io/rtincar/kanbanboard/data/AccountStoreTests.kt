212922608c160a82a61c87f4dd1d043fc6e87f71
rtincar <ul2mac@gmail.com>
2017-11-19 20:15:36 +0100

    [ARQ] - Configuración del proyecto: Se cargan datos antes de ejecutar tests

--- application/data/src/test/kotlin/io/rtincar/kanbanboard/data/AccountStoreTests.kt
+++ application/data/src/test/kotlin/io/rtincar/kanbanboard/data/AccountStoreTests.kt
@@ -3,13 +3,15 @@ package io.rtincar.kanbanboard.data
 import io.rtincar.kanbanboard.account.Account
 import io.rtincar.kanbanboard.account.AccountStore
 import io.rtincar.kanbanboard.account.DuplicateAccountException
+import io.rtincar.kanbanboard.data.accounts.AccountDocument
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.io.ResourceLoader
+import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 
@@ -17,14 +19,38 @@ import org.springframework.test.context.junit4.SpringRunner
 @ContextConfiguration(classes = arrayOf(DataConfiguration::class))
 class AccountStoreTests {
 
+
     @Autowired
     private lateinit var accountStore: AccountStore
+
+    @Autowired
+    private lateinit var mongoOperations: ReactiveMongoOperations
+
+    @Autowired
+    private lateinit var resourceLoader: ResourceLoader
+
+    @Before
+    fun setUp() {
+
+        // TODO: buscar forma de cargar datos desde JSON
+        /*
+        *
+        val resource = resourceLoader.getResource("classpath:data/accounts.json")
+        val objectMapper = ObjectMapper()
+
+        */
+
+        mongoOperations.insert(AccountDocument(null, "account1@domain.com", "John", "Doe", "password")).block()
+        mongoOperations.insert(AccountDocument(null, "account2@domain.com", "John", "Doe", "password")).block()
+        mongoOperations.insert(AccountDocument(null, "account3@domain.com", "John", "Doe", "password")).block()
+
+    }
 
     @Test
     fun `Should save an account and return account created with unique ID`() {
 
         //TODO: Continuar con el repositorio
-        val savedAccount = accountStore.save(Account(null, "account@domain.com", "John", "Doe", "password"))
+        val savedAccount = accountStore.save(Account(null, "account_new@domain.com", "John", "Doe", "password"))
 
         assertNotNull("Should return created account", savedAccount)
         assertNotNull("Account.id shouldn't be null", savedAccount.id)
@@ -34,12 +60,12 @@ class AccountStoreTests {
 
     @Test
     fun `Should find an account by email`() {
-        val exists = accountStore.exists("account@domain.com")
+        val exists = accountStore.exists("account1@domain.com")
         assertTrue("Should exist", exists)
     }
 
     @Test(expected = DuplicateAccountException::class)
     fun `Should throw DuplicateAccountData if there is another account with same email`() {
-        accountStore.save(Account(null, "account@domain.com", "John", "Doe", "password"))
+        accountStore.save(Account(null, "account1@domain.com", "John", "Doe", "password"))
     }
 }
